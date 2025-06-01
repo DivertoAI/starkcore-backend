d3dceb94f9e119b6bfcd11890bbe9737f984c815
Saswata Saha <appsmoothies@gmail.com>
2025-06-01 13:03:06 +0530

    run base url

--- app/ai/runpod_handler.py
+++ app/ai/runpod_handler.py
@@ -1,5 +1,5 @@
 from runpod.serverless.modules.rp_handler import RunpodHandler
-from app.ai.handler import handler
+from handler import handler
 
 # Start the serverless handler loop
 RunpodHandler(handler).start()
